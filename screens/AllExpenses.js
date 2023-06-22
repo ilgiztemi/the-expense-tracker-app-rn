1e981c1527b9a6499f412cf6ddb7bad22c571548
Kanykey Albanova <temirbekov91@gmail.com>
2023-06-21 22:12:37 -0400

    added dummy expense data

--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -1,14 +1,11 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 
 const AllExpenses = () => {
   return (
-    <View>
-      <Text>AllExpenses</Text>
-    </View>
+    <ExpensesOutput expensesPeriod="Total" />
   )
 }
 
 export default AllExpenses
 
-const styles = StyleSheet.create({})
