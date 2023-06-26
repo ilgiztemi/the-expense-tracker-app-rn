4885ed635cffaa540b909835fea4a73bbea1c914
Kanykey Albanova <temirbekov91@gmail.com>
2023-06-26 17:27:57 -0400

    built a custom input component

---
+++ components/ManageExpense/Input.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import React from 'react'
+
+const Input = ({label, textInputConfig}) => {
+  return (
+    <View>
+      <Text>{label}</Text>
+      <TextInput {...textInputConfig} />
+    </View>
+  )
+}
+
+export default Input
+
+const styles = StyleSheet.create({})
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -4,11 +4,11 @@ import { getDateMinusDays } from "../util/date";
 import { ExpensesContext } from "../store/expensesContext";
 
 const RecentExpenses = () => {
-  const expensesCtx = useContext(ExpensesContext);
+  const expensesCtx = useContext( ExpensesContext );
   const recentExpenses = expensesCtx.expenses.filter( expense => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays( today, 7 );
-    return (expense.date >= date7DaysAgo && expense.date <= today);
+    return ( expense.date >= date7DaysAgo );
   } );
   return (
     <ExpensesOutput expenses={ recentExpenses } expensesPeriod="Last 7 days" fallBackText="No expenses registered for the last 7 days." />
