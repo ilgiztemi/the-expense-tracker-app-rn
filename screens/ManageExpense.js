c01df0fcbce850be867854e2a8c2856e0906b12e
Kanykey Albanova <temirbekov91@gmail.com>
2023-06-26 17:07:55 -0400

    deleting / updating expenses

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,22 +1,28 @@
 import { StyleSheet, View } from 'react-native';
-import React, { useLayoutEffect } from 'react';
+import React, { useContext, useLayoutEffect } from 'react';
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/style";
 import Button from "../components/UI/Button";
+import { ExpensesContext } from "../store/expensesContext";
 
 const ManageExpense = ( { route, navigation } ) => {
+  const expensesCtx = useContext( ExpensesContext );
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
   const deleteExpenseHandler = () => {
+    expensesCtx.deleteExpense( editedExpenseId );
     navigation.goBack();
-
   };
   const cancelHandler = () => {
     navigation.goBack();
   };
   const confirmHandler = () => {
+    if ( isEditing ) {
+      expensesCtx.updateExpense( editedExpenseId, { description: 'Test!!!!', amount: 29.99, date: new Date( '2022-05-20' ) } );
+    } else {
+      expensesCtx.addExpense( { description: 'Test', amount: 19.99, date: new Date( '2022-05-19' ) } );
+    }
     navigation.goBack();
-
   };
   useLayoutEffect( () => {
     navigation.setOptions( {
@@ -26,8 +32,8 @@ const ManageExpense = ( { route, navigation } ) => {
   return (
     <View style={ styles.container }>
       <View style={ styles.buttons }>
-        <Button style={styles.button} mode='flat' onPress={ cancelHandler }>Cancel</Button>
-        <Button style={styles.button} onPress={confirmHandler}>{ isEditing ? 'Update' : 'Add' }</Button>
+        <Button style={ styles.button } mode='flat' onPress={ cancelHandler }>Cancel</Button>
+        <Button style={ styles.button } onPress={ confirmHandler }>{ isEditing ? 'Update' : 'Add' }</Button>
       </View>
       <View style={ styles.deleteContainer }>
         { isEditing && <IconButton icon='trash' color={ GlobalStyles.colors.error500 } size={ 36 } onPress={ deleteExpenseHandler } /> }
--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -8,7 +8,7 @@ const RecentExpenses = () => {
   const recentExpenses = expensesCtx.expenses.filter( expense => {
     const today = new Date();
     const date7DaysAgo = getDateMinusDays( today, 7 );
-    return expense.date > date7DaysAgo;
+    return (expense.date >= date7DaysAgo && expense.date <= today);
   } );
   return (
     <ExpensesOutput expenses={ recentExpenses } expensesPeriod="Last 7 days" />
