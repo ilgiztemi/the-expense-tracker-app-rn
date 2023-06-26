aa8eda5e3c22df3b5c667f2a9645c7e4fe68cce4
Kanykey Albanova <temirbekov91@gmail.com>
2023-06-25 21:19:49 -0400

    added custom buttons

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React, { useLayoutEffect } from 'react';
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/style";
+import Button from "../components/UI/Button";
 
 const ManageExpense = ( { route, navigation } ) => {
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
   const deleteExpenseHandler = () => {
+
+  };
+  const cancelHandler = () => {
+
+  };
+  const confirmHandler = () => {
 
   };
   useLayoutEffect( () => {
@@ -16,6 +23,10 @@ const ManageExpense = ( { route, navigation } ) => {
   }, [ navigation, isEditing ] );
   return (
     <View style={ styles.container }>
+      <View style={ styles.buttons }>
+        <Button style={styles.button} mode='flat' onPress={ cancelHandler }>Cancel</Button>
+        <Button style={styles.button} onPress={confirmHandler}>{ isEditing ? 'Update' : 'Add' }</Button>
+      </View>
       <View style={ styles.deleteContainer }>
         { isEditing && <IconButton icon='trash' color={ GlobalStyles.colors.error500 } size={ 36 } onPress={ deleteExpenseHandler } /> }
       </View>
@@ -30,6 +41,15 @@ const styles = StyleSheet.create( {
     flex: 1,
     padding: 24,
     backgroundColor: GlobalStyles.colors.primary800
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    minWidth: 120,
+    marginHorizontal: 8
   },
   deleteContainer: {
     marginTop: 16,
