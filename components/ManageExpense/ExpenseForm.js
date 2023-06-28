cdb3bde71021ac2cb047aa9c26b2123946ef8f87
Kanykey Albanova <temirbekov91@gmail.com>
2023-06-27 20:17:32 -0400

    handling user input in generic way

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,27 +1,41 @@
 import { StyleSheet, View, Text } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
 import Input from "./Input";
 
 const ExpenseForm = () => {
-  const amountChangedHandler = () => {
-
+  const [ inputValues, setInputValues ] = useState( {
+    amount: '',
+    date: '',
+    description: ''
+  } );
+  const inputChangedHandler = ( inputIdentifier, enteredValue ) => {
+    setInputValues( currInputValues => {
+      return {
+        ...currInputValues,
+        [ inputIdentifier ]: enteredValue
+      };
+    } );
   };
   return (
-    <View style={styles.form}>
-      <Text style={styles.title}>Your Expense</Text>
+    <View style={ styles.form }>
+      <Text style={ styles.title }>Your Expense</Text>
       <View style={ styles.inputsForm }>
-        <Input label="Amount" style={styles.rowInput} textInputConfig={ {
+        <Input label="Amount" style={ styles.rowInput } textInputConfig={ {
           keyboardType: 'decimal-pad',
-          onChangedText: amountChangedHandler
+          onChangedText: inputChangedHandler.bind( this, 'amount' ),
+          value: inputValues.amount
         } } />
-        <Input label="Date" style={styles.rowInput} textInputConfig={ {
+        <Input label="Date" style={ styles.rowInput } textInputConfig={ {
           placeholder: 'YYYY-MM-DD',
           maxLength: 10,
-          onChangedText: () => { }
+          onChangedText: inputChangedHandler.bind( this, 'date' ),
+          value: inputValues.date
         } } />
       </View>
       <Input label="Description" textInputConfig={ {
         multiline: true,
+        onChangedText: inputChangedHandler.bind( this, 'description' ),
+        value: inputValues.description
         // autoCapitalize: 'none',
         // autoCorrect: false  //default is true
       } } />
